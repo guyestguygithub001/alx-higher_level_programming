7d4488eca9f01314e4d591038aee018f35fa02a4
daniel ene <danielene050@gmail.com>
2023-10-02 14:31:18 -0700

    THank God ffor Python

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Python is an interpreted language, meaning that it's processed and executed by an interpreter, rather than being compiled to machine code"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
