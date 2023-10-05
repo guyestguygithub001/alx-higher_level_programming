5f6ef6de5c3a76402cb551f84f5f72f28e76965e
daniel ene <danielene050@gmail.com>
2023-10-05 08:30:03 -0700

    commit this message to Git.....

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function.
+
+    Arguments:
+        a: first int.
+        b: second int.
+
+    Returns:
+        return value. a + b
+    """
+    return (a + b)
