36f57be4899fb4a2a9a206764c606b13e728e886
daniel ene <danielene050@gmail.com>
2023-10-23 15:05:52 -0700

    I don tire for this thing

---
+++ 0x05-python-exceptions/python.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <python.h>
+
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
+def safe_print_list_integers(my_list=[], x=0):
+def safe_print_division(a, b):
+def list_division(my_list_1, my_list_2, list_length):
+def raise_exception():
+def raise_exception_msg(message=""):
+def safe_print_integer_err(value):
+def safe_function(fct, *args):
+void print_python_list(PyObject *p);
+
+#endif
