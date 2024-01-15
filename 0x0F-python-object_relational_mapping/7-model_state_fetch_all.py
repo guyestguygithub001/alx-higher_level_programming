096b670c2d29a4256d253f6b167229bcac7fd261
guyestguygithub001 <danielene050@gmail.com>
2024-01-14 20:32:36 -0800

    FOR ORM

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""init link class to table in our database
+"""
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
