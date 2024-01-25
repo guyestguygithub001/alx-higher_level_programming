e26208e99685ac37d3a592b278c90dc3bc4e7dd0
guyestguygithub001 <danielene050@gmail.com>
2024-01-25 14:07:10 -0800

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request to the URL, and displays the body of the response if the status code is 200
-curl -sL -w "%{http_code}\\n" "$1" -o /dev/null | grep -q 200 && curl -s "$1"
+curl -Ls "$1"
 
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a GET request to the URL with a header variable, and displays the body of the response
-curl -s -H "X-School-User-Id: 98" "$1"
-
+# This script takes a URL as an argument, sends a GET request to the URL with a header variable, and displays the body of the respo
+curl -sH "X-School-User-Id: 98" "${1}"
