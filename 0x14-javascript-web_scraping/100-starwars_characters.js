f245c2ce4756a343c33dc56f06a2a03d59a390b8
guyestguygithub001 <danielene050@gmail.com>
2024-02-20 14:22:10 -0800

    recheck

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,35 +1,28 @@
 #!/usr/bin/node
-// Import the 'request' module for making HTTP requests
+
 const request = require('request');
 
-// Define a function that fetches and prints the characters of a Star Wars movie
-function getCharacters(movieId) {
-  // Construct the URL for the API request
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-
-  // Make a request to the Star Wars API to get the data of the specified movie
-  request(url, function (error, response, body) {
-    // If the request was successful (no errors and status code 200), proceed
-    if (!error && response.statusCode == 200) {
-      // Parse the body of the response (which is in JSON format) to a JavaScript object
-      const movie = JSON.parse(body);
-
-      // For each character URL in the movie data, make another request to get the character's data
-      movie.characters.forEach((characterUrl) => {
-        request(characterUrl, function (error, response, body) {
-          // If the request was successful, print the name of the character
-          if (!error && response.statusCode == 200) {
-            const character = JSON.parse(body);
-            console.log(character.name);
-          }
-        });
-      });
-    }
-  });
-}
-
-// Get the movie ID from the command-line arguments
 const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-// Call the function with the specified movie ID
-getCharacters(movieId);
+request(apiUrl, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+
+    console.log(`Characters of "${movieData.title}":`);
+
+    movieData.characters.forEach((characterUrl) => {
+      request(characterUrl, function (charError, charResponse, charBody) {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+
+          console.log(characterData.name);
+        } else {
+          console.error('Error fetching character data:', charError);
+        }
+      });
+    });
+  } else {
+    console.error('Error fetching movie data:', error);
+  }
+});
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,47 +1,47 @@
 #!/usr/bin/node
 
-// Import the 'request' module
+// Importing the 'request' module
 const request = require('request');
 
-// Get the URL from the command line arguments
-const url = process.argv[2];
+// Getting the API URL from the command line arguments
+const apiUrl = process.argv[2];
 
-// Make a request to the specified URL
-request(url, function (err, response, body) {
-  // If there's an error, log it
-  if (err) {
-    console.log(err);
-  } 
-  // If the response status code is 200 (OK), process the response
-  else if (response.statusCode === 200) {
-    // Initialize an object to store the completed tasks
-    const completed = {};
+// Making a request to the API
+request(apiUrl, function (error, response, body) {
+  // Checking if there is no error and the status code is 200 (OK)
+  if (!error && response.statusCode === 200) {
+    try {
+      // Parsing the body of the response to a JSON object
+      const todos = JSON.parse(body);
 
-    // Parse the body of the response into a JavaScript object
-    const tasks = JSON.parse(body);
+      // Creating an object to store the completed tasks count for each user
+      const completed = {};
 
-    // Iterate over each task
-    for (const i in tasks) {
-      const task = tasks[i];
-
-      // If the task is completed
-      if (task.completed === true) {
-        // If this is the first completed task for this user, initialize their count to 1
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } 
-        // Otherwise, increment their count
-        else {
-          completed[task.userId]++;
+      // Iterating over each todo item
+      todos.forEach((todo) => {
+        // Checking if the todo item is completed
+        if (todo.completed) {
+          // If the user has not been added to the completed object, add them with a count of 1
+          // Otherwise, increment the count of completed tasks for the user
+          if (completed[todo.userId] === undefined) {
+            completed[todo.userId] = 1;
+          } else {
+            completed[todo.userId]++;
+          }
         }
-      }
-    }
+      });
 
-    // Log the completed task counts
-    console.log(completed);
-  } 
-  // If the response status code is anything other than 200, log an error message
-  else {
-    console.log('An error occurred. Status code: ' + response.statusCode);
+      // Formatting the completed object to a string for output
+      const output = `{${Object.entries(completed).map(([key, value]) => ` '${key}': ${value}`).join(',\n ')} }`;
+
+      // If there are more than 2 users, output the formatted string, otherwise output the completed object
+      console.log(Object.keys(completed).length > 2 ? output : completed);
+    } catch (parseError) {
+      // If there is an error parsing the JSON, log the error
+      console.error('Error parsing JSON:', parseError);
+    }
+  } else {
+    // If there is an error with the request, log the error
+    console.error('Error:', error);
   }
 });
